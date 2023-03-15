304126856b6972c72a797c95e7f8d127a879d083
Kunguma Sakthivel K <93455647+kungumasakthivel@users.noreply.github.com>
2023-03-15 16:12:40 +0530

    final cleanup

---
+++ guvi-task/user_page.php
@@ -0,0 +1,40 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name']) && $_SESSION['']){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>user page</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/admin_page.css" />
+</head>
+<body>
+   
+<div class="container">
+
+   <div class="content">
+      <h3>Hi, <span><?php echo $_SESSION['user_name'] ?></span></h3>
+      <br>
+      <a href="login_form.php" class="btn">login</a>
+      <a href="register_form.php" class="btn">register</a>
+      <a href="logout.php" class="btn">logout</a>
+   </div>
+
+</div>
+
+</body>
+</html>
